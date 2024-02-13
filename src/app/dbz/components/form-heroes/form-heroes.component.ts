e8cb9d9730f51ee35809a054446362ef72da9a5e
Johan <johanoriginalplus@gmail.com>
2024-02-13 16:39:32 -0500

    Asignanddo un valor por defecto a un form

--- src/app/dbz/components/form-heroes/form-heroes.component.ts
+++ src/app/dbz/components/form-heroes/form-heroes.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-form-heroes',
   templateUrl: './form-heroes.component.html',
   styleUrl: './form-heroes.component.css',
 })
-export class FormHeroesComponent {}
+export class FormHeroesComponent {
+  public character: Character = { name: 'Vegeta', power: 7000 };
+}
